00e24ad9d25aa484baf6787152c0c5c87a55bf0e
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2022-01-27 13:41:57 +0300

    урок 7 заработало, дело было в http без s, в одной букве

--- app/src/main/java/com/example/lessonweather/utills/const.kt
+++ app/src/main/java/com/example/lessonweather/utills/const.kt
@@ -7,7 +7,7 @@ const val BUNDLE_KEY_LON = "key_lon"
 const val BROADCAST_ACTION = "BROADCAST_ACTION"
 const val DETAILS_INTENT_FILTER = "DETAILS INTENT FILTER"
 
-const val YANDEX_API_URL = "http://api.weather.yandex.ru/"
+const val YANDEX_API_URL = "https://api.weather.yandex.ru/"
 const val YANDEX_API_URL_END_POINT = "v2/informers"
 
 const val YANDEX_API_KEY =  "X-Yandex-API-Key"
